d8fb0b4909239bc5e876ed8767f72f4155f45c58
Shehab Ahmed <just.shiba3006@gmail.com>
2024-06-28 22:25:42 +0300

    test commit

--- lib/secret_keys.dart
+++ lib/secret_keys.dart
@@ -1,5 +1,7 @@
 class SecretKeys {
-  static const String googleMapApiAndroid = 'AIzaSyCE9_UMPi2JhUDE8edIbM6WRPAdWqs_bcU';
-  static const String googleMapApiIos = 'AIzaSyBEZvjWqcrmlqKVyqlQORx7VGwuZr1whrc';
-  static const String googleMapApiweb = 'r1whrc';
+  static const String googleMapApiAndroid =
+      'AIzaSyCE9_UMPi2JhUDE8edIbM6WRPAdWqs_bcU';
+  static const String googleMapApiIos =
+      'AIzaSyBEZvjWqcrmlqKVyqlQORx7VGwuZr1whrc';
+  static const String googleMapApiweb = 'r1whrcaaaaaaaaaaaaaaaaaaaaa';
 }

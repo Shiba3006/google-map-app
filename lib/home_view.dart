f704db2b99bd51af82014d6f46cb0be2e1a1c568
Shehab Ahmed <just.shiba3006@gmail.com>
2024-06-28 22:59:03 +0300

    test google map widget

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late CameraPosition initialCameraPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    initialCameraPosition = const CameraPosition(
+        target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GoogleMap(initialCameraPosition: initialCameraPosition);
   }
 }

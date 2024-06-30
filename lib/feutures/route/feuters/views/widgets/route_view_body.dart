bfb79849b07f49142f8026dd18ef88469bb04aa8
Shehab Ahmed <just.shiba3006@gmail.com>
2024-06-30 16:38:05 +0300

    added marker to my location

--- lib/feutures/route/feuters/views/widgets/route_view_body.dart
+++ lib/feutures/route/feuters/views/widgets/route_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_map_app/core/utils/exceptions.dart';
+import 'package:google_map_app/core/utils/location_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class RouteViewBody extends StatefulWidget {
   const RouteViewBody({super.key});
@@ -10,17 +13,27 @@ class RouteViewBody extends StatefulWidget {
 
 class _RouteViewBodyState extends State<RouteViewBody> {
   late CameraPosition initialCameraPosition;
+  late LocationService location;
+  late GoogleMapController googleMapController;
+  Set<Marker> markers = {};
+
   @override
   void initState() {
     super.initState();
     initCameraPosition();
+    location = LocationService(location: Location());
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       zoomControlsEnabled: false,
+      markers: markers,
       initialCameraPosition: initialCameraPosition,
+      onMapCreated: (controller) {
+        googleMapController = controller;
+        updateCurrentLocation();
+      },
     );
   }
 
@@ -29,5 +42,42 @@ class _RouteViewBodyState extends State<RouteViewBody> {
       target: LatLng(29.99552098735423, 31.205921201848618),
       zoom: 1,
     );
+  }
+
+  void updateCurrentLocation() async {
+    try {
+      LocationData locationData = await location.getLocation();
+      LatLng myLocationPosition = LatLng(
+        locationData.latitude!,
+        locationData.longitude!,
+      );
+      addMarkerToMyLocation(myLocationPosition);
+      await animateCameraToMyLocation(myLocationPosition);
+    } on LocationServiceException catch (e) {
+      // TODO
+    } on LocationPermissioneException catch (e) {
+      // TODO
+    } catch (e) {
+      // TODO
+    }
+  }
+
+  void addMarkerToMyLocation(LatLng myLocationPosition) {
+    Marker myLocationMarker = Marker(
+      markerId: const MarkerId('myLocation'),
+      position: myLocationPosition,
+      infoWindow: const InfoWindow(title: 'My Location'),
+    );
+    markers.add(myLocationMarker);
+    setState(() {});
+  }
+
+  Future<void> animateCameraToMyLocation(LatLng myLocationPosition) async {
+    CameraPosition myCameraPosition = CameraPosition(
+      target: myLocationPosition,
+      zoom: 16,
+    );
+    await googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(myCameraPosition));
   }
 }

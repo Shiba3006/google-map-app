af6737b4f7b039c488caeb36f4f5fa0873083016
Shehab Ahmed <just.shiba3006@gmail.com>
2024-07-01 19:53:45 +0300

    created routes  model

--- lib/core/utils/routes_service.dart
+++ lib/core/utils/routes_service.dart
@@ -3,6 +3,4 @@ import 'package:google_map_app/core/utils/api_service.dart';
 class RoutesService {
   RoutesService({required ApiService apiService}) : _apiService = apiService;
   final ApiService _apiService;
-
-  
 }

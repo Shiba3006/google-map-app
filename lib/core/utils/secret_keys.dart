9406283770e7e82a59d49e343210307833956803
Shehab Ahmed <just.shiba3006@gmail.com>
2024-07-01 16:19:07 +0300

    integrate uuid in ui

--- lib/core/utils/secret_keys.dart
+++
@@ -1,8 +0,0 @@
-class SecretKeys {
-  static const String googleMapApiKeyAndroid =
-      'AIzaSyCE9_UMPi2JhUDE8edIbM6WRPAdWqs_bcU';
-  static const String googleMapApiKeyIos =
-      'AIzaSyBEZvjWqcrmlqKVyqlQORx7VGwuZr1whrc';
-  static const String placesRequestApiKey =
-      'AIzaSyC1q4qHVjcLKBBHPcSlD-F1xd6y_LL8TLI';
-}
